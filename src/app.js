6c80e6901a6f5824d5a551b40a1ecf9cad4ebb8f
valeriapaulinalustres <valeriapaulinalustres@yahoo.com.ar>
2023-03-16 06:52:01 -0300

    first commit

---
+++ src/app.js
@@ -0,0 +1,72 @@
+import express from 'express'
+import './persistence/dbConfig.js'
+import cookieParser from 'cookie-parser'
+import session from 'express-session'
+import mongoStore from 'connect-mongo'
+import cors from 'cors'
+
+import usersRouter from './persistence/routes/users.router.js'
+
+const app = express()
+app.use(cors())
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+
+//cookie parser (para guardar id de session)
+app.use(cookieParser())
+
+
+// Session con Mongo
+app.use(
+  session({
+    secret: 'sessionKey',
+    resave: false,
+    saveUninitialized: true,
+    store: new mongoStore({
+      mongoUrl: 'mongodb+srv://valeriapaulinalustres:Artemisa37@cluster0.knm2ak6.mongodb.net/epi?retryWrites=true&w=majority'
+    }),
+  })
+)
+
+//rutas
+app.use('/users', usersRouter)
+
+
+
+app.listen(8082, () => {
+    console.log('Servidor escuchando en el puerto 8082');
+})
+/*
+
+
+
+import path from 'path'
+
+
+import { __dirname } from './utils.js'
+import handlebars from 'express-handlebars'
+
+
+
+
+
+
+
+
+
+
+
+
+
+//console.log(__dirname)
+
+// archivos estaticos
+//OJO QUE DETRÁS DE PUBLIC NO HAY BARRA, POR LO QUE DONDE SE NECESITE SEGUIR CON LA URL (EJEMPLO STYLE.CSS) HAY QUE PONERLE LA BARRA DELANTE
+app.use(express.static(path.join(__dirname, '/public')))
+
+
+
+
+  */
