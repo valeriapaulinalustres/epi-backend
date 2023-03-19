7455b2d4a94eb9e869ff6620e69471e87e8c7988
valeriapaulinalustres <valeriapaulinalustres@yahoo.com.ar>
2023-03-19 13:45:33 -0300

    cors

--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ app.use(express.urlencoded({ extended: true }))
 
 //para evitar bloqueo de CORS
 app.use((req,res,next)=>{
-  res.setHeader('Access-Control-Allow-Origin','*');
+  res.setHeader('Access-Control-Allow-Origin','https://epi-six.vercel.app');
   res.setHeader('Access-Control-Allow-Methods','GET,POST,PUT,PATCH,DELETE');
   res.setHeader('Access-Control-Allow-Methods','Content-Type','Authorization');
   next(); 
