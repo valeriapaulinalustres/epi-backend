0d0e3e340b2bff5a5f177f1e23803c4c22fabdba
valeriapaulinalustres <valeriapaulinalustres@yahoo.com.ar>
2023-03-19 10:10:39 -0300

    cors

--- src/app.js
+++ src/app.js
@@ -14,6 +14,14 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
+//para evitar bloqueo de CORS
+app.use((req,res,next)=>{
+  res.setHeader('Access-Control-Allow-Origin','*');
+  res.setHeader('Access-Control-Allow-Methods','GET,POST,PUT,PATCH,DELETE');
+  res.setHeader('Access-Control-Allow-Methods','Content-Type','Authorization');
+  next(); 
+})
+
 
 //cookie parser (para guardar id de session)
 app.use(cookieParser())
